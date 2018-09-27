e34b6db52e5404dd749ee9712eafd20b4c12c9be
Nitish Ranjan <nitish.ranjan03@gmail.com>
2018-09-27 16:05:30 -0400

    add cache files

--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
+const expectCaches = 'restaurant-v1';
+
+const cacheFolder = [
+  './',
+  './index.html',
+  './restaurant.html',
+  './css/styles.css',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './js/dbhelper.js',
+  './data/restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg'
+];
+
 self.addEventListener('install', event => {
   console.log('installing!');
 });
