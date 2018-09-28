1f4a56e1f938a0b8b1d67b93ef0a0a0e599db5f4
Nitish Ranjan <nitish.ranjan03@gmail.com>
2018-09-27 21:47:08 -0400

    fetching application offline

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 const expectCaches = ['restaurant-v1'];
 
+// array of cache files
 const cacheFolder = [
   './',
   './index.html',
@@ -21,12 +22,26 @@ const cacheFolder = [
   './img/10.jpg'
 ];
 
+// install event listener and caching the application shell
 self.addEventListener('install', event => {
-  console.log('installing!');
+  // console.log('installing!');
+
   event.waitUntil(
     caches.open(expectCaches).then(cache => {
-      console.log(cacheFolder);
+      // console.log(cacheFolder);
+
       return cache.addAll(cacheFolder);
+    })
+  );
+});
+
+// fetch event listener making application offline
+self.addEventListener('fetch', event => {
+// console.log('ready for fetching offline');
+
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
